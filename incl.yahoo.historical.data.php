1184cedc245625bdbcd29071d76b7d4fe503a20f
Scott Eickhoff <seickhoff@paypal.com>
2017-06-22 22:33:17 -0500

    added column selection and ordering

--- incl.yahoo.historical.data.php
+++ incl.yahoo.historical.data.php
@@ -7,25 +7,37 @@
 	Input
 		array(
 			'symbol' => "symbol string",
-			'start_date' => "m/d/y",
-			'end_date' => "m/d/y",
-			'additional_days_back' =>  X (offset the start_date by X days)
-		)	
-
-	Output
-		array(
-			"symbol,date,volume,high,low,open,close",
-			...
+			'start' => "m/d/y",
+			'end' => "m/d/y",
+			'offset' =>  X, 
+			'columns' => array('s', 'd', 'v', 'h', 'l', 'o', 'c', 'a')	
 		)
+		
+		NOTES: 
+			- optional: use 'offset' to set the start date X days prior.
+			- optional: use 'columns' to indicate the fields and order to return.
+				- default: s (symbol), d (date), v (volume), 'h', (high), 'l' (low), 'o' (open), 'c' (close)
+			- The Adjusted Close is used to correct the other values.
+	Output
+		array of CSV rows in chronologically ascending order
 */
 function yahoo($param) {
 	
 	$cookie = get_cookie();
 	$crumb = get_crumb($cookie);
 	
-	$start = strtotime("{$param['start']} midnight -{$param['offset']} day" );
-	$end = strtotime("{$param['end']} midnight +1 day" );
+	// defaults
+	$symbol = (isset($param['symbol'])) ? $param['symbol'] : 'AAPL';
 
+	$end = (isset($param['end'])) ? $param['end'] : date('Y-m-d', strtotime("now"));
+	$start = (isset($param['start'])) ? $param['start'] : date('Y-m-d', strtotime("{$end} -14 day"));
+
+	$offset = (isset($param['offset'])) ? $param['offset'] : 0;
+	$arr_col = (isset($param['columns'])) ? $param['columns'] : array('s', 'd', 'v', 'h', 'l', 'o', 'c');
+	
+	$start = strtotime("{$start} midnight -{$offset} day" );
+	$end = strtotime("{$end} midnight +1 day" );
+	
 	// get CSV data
 	$result = curl(array(
 		"url" => "https://query1.finance.yahoo.com/v7/finance/download/{$param['symbol']}?period1={$start}&period2={$end}&interval=1d&events=history&crumb={$crumb}",
@@ -56,7 +68,25 @@ function yahoo($param) {
 		$c = $a * $factor;
 		$v = $v / $factor;
 		
-		$arr_temp[] = "{$param['symbol']},{$d},{$v},{$h},{$l},{$o},{$c}";
+		$data = array(
+			"a" => $a,
+			"s" => $param['symbol'],
+			"d" => $d,
+			"o" => $o,
+			"c" => $c,
+			"h" => $h,
+			"l" => $l,
+			"v" => $v
+		); 
+		
+		$line = array();
+		
+		foreach ($arr_col as $col) {
+			$line[] = $data[$col];
+		}
+		
+		$arr_temp[] = join(',', $line);
+		
 	} 
 	return $arr_temp;
 }
